09cc8ebd6359c6a14b9a3ffadc6a72e863924d1c
473042120 <473042120@qq.com>
2020-07-09 13:40:27 +0800

    master

--- c/.h/digui.h
+++ c/.h/digui.h
@@ -8,3 +8,16 @@ int jieji (int n)
     return (n*jieji(n-1));
 }
 
+int max (int a[],int l,int h)
+{
+    int m,x,y;
+    if (l=h)
+    return a[1];
+    m=(1+h)/2;
+    x=max(a,1,m);
+    y=max(a,m+1,h);
+    if(x>y)
+    return x;
+    else 
+    return y;
+}
--- c/code/digui.c
+++ c/code/digui.c
@@ -1,8 +1,10 @@
-# include ".h/digui.h"
+# include "/home/e575/GIT/.h/digui.h"
 
 void main()
 {
    int n;
+   int a[6]={1,2,3,4,5,6};
    scanf("%d",&n);
    printf("%d的阶是%d\n",n,jieji(n));
+   printf("%d", max(a[6],3,5));
 }
