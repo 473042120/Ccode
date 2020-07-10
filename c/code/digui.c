a0f70adcb31e7e0261e189ce1468770d9aa055e3
473042120 <473042120@qq.com>
2020-07-10 17:57:29 +0800

    master

--- c/code/digui.c
+++ c/code/digui.c
@@ -6,6 +6,6 @@ void main()
    // printf("%d的阶是%d\n",o,jieji(o));
    // printf("%d\n",max(a,0,7));
   // printf("%d\n",Train_into_PlatForm(7,0));
-    printf("%d",dunno(3));
+    printf("%d\n",dunno(3));
 }
    
