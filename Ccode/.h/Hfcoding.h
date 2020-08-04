a39c6089a5d5747b5828fb8e157649e3d6b2822d
473042120 <473042120@qq.com>
2020-08-05 06:45:23 +0800

    mater

---
+++ Ccode/.h/Hfcoding.h
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define N 20
+typedef int DataType;
+typedef struct {
+	char ch;
+	DataType weight;
+	int lchild ,rchild,parent;
+}Htnode; 
+
+typedef struct {
+	char *code;
+	char leaf;
+	int length;
+}CodeType;
+
+void HuFcoding(Htnode hufftree[],CodeType cd[],int w[],int n)
+{
+	/*静态链表huftrre中，w存放结点权重，
+	n是叶子个数，最后的编码放在cd[] */
+	int i,j,k,s1,s2,s,m,f,c,sum;
+	char temp[N];
+	m=2*n-1;
+	for(i=1;i<n;i++)
+	{
+		huftree[i].weight=w[i-1];
+	}
+ } 
--- Ccode/.h/QBRree.h
+++ Ccode/.h/QBRree.h
@@ -3,6 +3,10 @@
 #define MaxTreeNodeNum 100
 
 typedef char DataType;
+void Visit(char t)
+{
+   printf("%c",t);
+}
 typedef struct {
     DataType data[MaxTreeNodeNum];
     int n;
@@ -12,11 +16,6 @@ typedef struct bonde{
     DataType data;
     struct bonde *lchild,*rchild;
 }Bonde,*BTree;
-
-void Visit(DataType t)
-{
-   
-}
 
 void PreOrder (BTree t)//先序遍历的递归算法
 {
