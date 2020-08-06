fbc3abd1d6a7b123c171afc3649b53681ea9aa71
473042120 <473042120@qq.com>
2020-08-06 21:23:01 +0800

    mster

--- Ccode/.h/邻接矩阵.h
+++ Ccode/.h/邻接矩阵.h
@@ -3,13 +3,13 @@
 #include<stdlib.h>
 #define MaxVertexNum 30 //最大顶点数
 
-typedef struct{
+typedef int
 //顶点表的类型
-}VertexType;
+VertexType;
 
-typedef struct{
+typedef 
 //邻接矩阵，既边表
-}Edgetype;
+int Edgetype;
 
 typedef struct{
     VertexType vertexs[MaxVertexNum];
@@ -21,4 +21,14 @@ void CreatGraph(MGragh * G)
 {
     int i,j,k,w;
     scanf("%d,%d",&(G->VertexNum),&(G->edgeNum));
+    for(i=0;i<G->VertexNum;i++)    
+    scanf("%c",&(G->vertexs[i]));
+    for(i=0;i<G->VertexNum;i++)  
+    for(j=0;j<G->VertexNum;i++)
+    G->arcs[i][j]=0;
+    for(k=0;k<G->edgeNum;k++)
+    {
+        scanf("%d,%d",&i,&j);
+        G->arcs[i][j];
+    }
 }
