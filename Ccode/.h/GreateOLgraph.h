454e2cc9c6a4e7ab2d5e332bedb6f8b138d51551
473042120 <473042120@qq.com>
2020-08-06 09:50:43 +0800

    master

--- Ccode/.h/GreateOLgraph.h
+++ Ccode/.h/GreateOLgraph.h
@@ -5,7 +5,7 @@
 
 typedef int InfoType;
 typedef int VertexType;
-int * LocateVertex (OLGraph * G,int v)
+int LocateVertex (OLGraph * G,int v)
 {
     /*确定v1和v2在G中位值*/
 }
@@ -18,7 +18,7 @@ typedef struct ArcNode{
 
 typedef struct VertexNode{
     VertexType vertex;
-    ArcNode*fisrin,*firstout;
+    ArcNode*firstin,*firstout;
 }VertexNode;
 
 typedef struct{
@@ -34,13 +34,17 @@ void CreateOLgraph(OLGraph * G)
     for(i=0;i<G->vertexNum;i++)
     {
         scanf("%d",&(G->xlist[i].firstout=NULL));
-        G->xlist[i].fisrin =NULL;G->xlist[i].firstout=NULL;
+        G->xlist[i].firstin =NULL;G->xlist[i].firstout=NULL;
     }
     for(k=0;k<G->edgeNum ;k++)
-    {   int *i,*j,v1,v2;
+    {   int i,j,v1,v2;
+        ArcNode *p;
         scanf("%d,%d",&v1,&v2);
         i=LocateVertex(G,v1);
         j=LocateVertex(G,v2); /*确定v1和v2在G中位值*/
-        
+        p=(ArcNode*)malloc(sizeof(ArcNode));
+        p->tailvertex=i;p->headvertex=j;
+        p->tlink=G->xlist[i].firstout;G->xlist[i].firstout=p;
+        p->hlink=G->xlist[j].firstin;G->xlist[j].firstin=p;
     }
 }
