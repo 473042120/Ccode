b6e3e78ff99b95739b85fe8899c5129f28032d6b
473042120 <473042120@qq.com>
2020-08-06 08:29:57 +0800

    master

--- Ccode/code.c
+++ Ccode/code.c
@@ -1,6 +1,8 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<malloc.h>
+#include <math.h>
 #include ".h/code.h"
-
-
 
 // int main()
 // {
@@ -61,4 +63,136 @@
 //  fun(x);
 //  printf("%d",x);
 // }
+
+//int main ()
+//{
+//int a=123;
+//printf("%d,%d,%d",a/100,a/10%10,a%10);
+//}
+
+
+//int main()
+//{   int i=0;
+//	char a[10];
+// for(i=0;i<10;i++)
+//   { 
+//   	scanf("%c",&a[i]);
+//   	if(a[i]=='\n') break;
+//   }
+//   for(i=0;i<10&&a[i]!='\n';i++)
+//   {
+//   	printf("%c",a[i]);
+//   }
+//   printf("%%");
+// } 
+
+
+//int main()
+//{
+//int a=1;
+//a*=a+=a-=1*3*a;
+//printf("%d",a);
+//}
+
+//
+//int main()
+//{
+//int a=1.6;
+//printf("%d",a);
+//}
+
+//int main()
+//{
+//double a=163.40902;
+//printf("%13.3e",a);
+//}
+//   1.634e+002结果前面有空格 
+
+//int main()
+//{
+//   long long int  a;
+//	a=1234567891;
+//	printf("%d",a);
+//}
+
+//int main()
+//{
+//	char a,b,c;
+//	a=getchar();
+//	b=getchar();
+//	c=getchar();
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	putchar(getchar());
+//	putchar(getchar());
+//	putchar(getchar());
+//	putchar('\n');
+//	return 0;
+//}
+
+//int main()
+//{int i=0;
+//	char a[10];
+//	for(i=0;i<10;i++)
+//	*(a+i)=getchar();
+//	for(i=0;i<10;i++)
+//	putchar(a[i]);
+//}
+
+//char max ()
+//{
+//	printf("%c",getchar()-32);
+// } 
+// main()
+//{  if(getchar()=='1')
+//	max();
+//	else
+//	min();
+//	return 0;
+//}
+
+
+//main()
+//{
+//	int a=1;
+//switch(a)
+//{
+//case 1:printf("%d ",a++);
+//case 2:printf("%d ",a++);break;
+//case 3:printf("%d ",a++);break;
+//default:printf("%d",a++);
+//}
+//printf("%d",a);
+//}
+//
+//int main()
+//{   
+//	printf("%f",fabs(-12.3));
+//}
+
+//int  f(int b)
+//{
+//	static int a=1;    
+//		a=a+1;
+//	return a;
+//}
+//
+//int main()
+//{  int i=0;
+//	while (i<10)
+//	{
+//		f(1);
+//		i++;
+//	}
+//	printf("%d",f(1));
+//}
+
 
